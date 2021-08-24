997b976e14516fd07a5f1c46328a6429450e14b8
Leak <sethpowellbusiness@gmail.com>
2021-08-24 14:04:09 -0700

    added getPortfolioItems func to portfolio-manager page to query database

--- src/components/Pages/portfolio-manager.js
+++ src/components/Pages/portfolio-manager.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PortfolioManager extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            portfolioItems: []
+        };
+    }
+
+    getPortfolioItems() {
+        axios.get("https://sethpowell.devcamp.space/portfolio/portfolio_items", {
+            withCredentials: true
+        }).then(response => {
+            this.setState({
+                portfolioItems: [...response.data.portfolio_items]
+            })
+        }).catch(error => {
+            console.log("Error in getPortfolioItems", error)
+        })
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems()
+    }
+
     render() {
         return (
             <div className="portfolio-manager-wrapper">
