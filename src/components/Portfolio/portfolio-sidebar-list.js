9eea62355a1d12323963f20c084f222f1421bd52
Leak <sethpowellbusiness@gmail.com>
2021-08-24 14:31:04 -0700

    added scss page for portfolio-sidebar-list and added basic styles

--- src/components/Portfolio/portfolio-sidebar-list.js
+++ src/components/Portfolio/portfolio-sidebar-list.js
@@ -3,12 +3,12 @@ import React from 'react';
 const PortfolioSidebarList = (props) => {
     const portfolioList = props.data.map(portfolioItem => {
         return (
-            <div>
-                <div>
+            <div className="portfolio-item-thumb">
+                <div className="portfolio-item-thumb-img">
                     <img src={portfolioItem.thumb_image_url} alt="thumb image" />
                 </div>
                 <div>
-                    <h1>{portfolioItem.name}</h1>
+                    <h1 className="title">{portfolioItem.name}</h1>
                     <h2>{portfolioItem.id}</h2>
                 </div>
             </div>
@@ -16,7 +16,7 @@ const PortfolioSidebarList = (props) => {
     });
 
 
-    return <div>{portfolioList}</div>;
+    return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
 }
 
 export default PortfolioSidebarList;
