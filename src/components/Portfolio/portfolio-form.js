977e739c73f07786632d44f59fc97e7939758c99
Leak <sethpowellbusiness@gmail.com>
2021-08-24 14:52:39 -0700

    built out form submission handlers and bound them

---
+++ src/components/Portfolio/portfolio-form.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class PortfolioForm extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Portfolio Form</h1>
+            </div>
+        )
+    }
+}
