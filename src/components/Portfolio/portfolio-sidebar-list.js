a8403a12d02d48437e3414faf4fb5efc33651585
Leak <sethpowellbusiness@gmail.com>
2021-08-26 15:06:33 -0700

    imported fontawesome and added an icon to delete button in portfolio sidebar

--- src/components/Portfolio/portfolio-sidebar-list.js
+++ src/components/Portfolio/portfolio-sidebar-list.js
@@ -1,20 +1,28 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PortfolioSidebarList = (props) => {
-  const portfolioList = props.data.map((portfolioItem) => {
-    return (
-      <div key={portfolioItem.id} className="portfolio-item-thumb">
-        <div className="portfolio-item-thumb-img">
-          <img src={portfolioItem.thumb_image_url} alt="thumb image" />
-        </div>
-        <h1 className="title">{portfolioItem.name}</h1>
-        <h2>{portfolioItem.id}</h2>
-        <a onClick={() => props.handleDeleteClick(portfolioItem)}>Delete</a>
-      </div>
-    );
-  });
+    const portfolioList = props.data.map((portfolioItem) => {
+        return (
+            <div key={portfolioItem.id} className="portfolio-item-thumb">
+                <div className="portfolio-item-thumb-img">
+                    <img
+                        src={portfolioItem.thumb_image_url}
+                        alt="thumb image"
+                    />
+                </div>
+                <h1 className="title">{portfolioItem.name}</h1>
+                <h2>{portfolioItem.id}</h2>
+                <a onClick={() => props.handleDeleteClick(portfolioItem)}>
+                    <FontAwesomeIcon icon="trash" />
+                </a>
+            </div>
+        );
+    });
 
-  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
+    return (
+        <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>
+    );
 };
 
 export default PortfolioSidebarList;
