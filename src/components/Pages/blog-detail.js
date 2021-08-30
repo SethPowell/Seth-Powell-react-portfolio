9294f7e831f8e7ae06e1feb47efad3132587bae8
Leak <sethpowellbusiness@gmail.com>
2021-08-30 16:25:17 -0700

    blog-detail page now runs a get request to get all info fo current blog item

--- src/components/Pages/blog-detail.js
+++ src/components/Pages/blog-detail.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogDetail extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			currentId: this.props.match.params.slug,
+			blogItem: {},
+		};
+	}
+
+	getBlogItem() {
+		axios
+			.get(
+				`https://sethpowell.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+			)
+			.then((response) => {
+				console.log("response", response);
+			})
+			.catch((error) => {
+				console.log("error in getBlogItem", error);
+			});
+	}
+
+	componentDidMount() {
+		this.getBlogItem();
 	}
 
 	render() {
