a2fea7dbcb3f0296080437da140d37d06e4d7984
Leak <sethpowellbusiness@gmail.com>
2021-08-30 16:43:28 -0700

    blog-detail now dynamically renders that blogItems title content and image

--- src/components/Pages/blog-detail.js
+++ src/components/Pages/blog-detail.js
@@ -17,7 +17,9 @@ export default class BlogDetail extends Component {
 				`https://sethpowell.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
 			)
 			.then((response) => {
-				console.log("response", response);
+				this.setState({
+					blogItem: response.data.portfolio_blog,
+				});
 			})
 			.catch((error) => {
 				console.log("error in getBlogItem", error);
@@ -29,6 +31,15 @@ export default class BlogDetail extends Component {
 	}
 
 	render() {
-		return <div className="">blog detail component</div>;
+		const { title, content, featured_image_url, blog_status } =
+			this.state.blogItem;
+
+		return (
+			<div className="">
+				<h1>{title}</h1>
+				<img src={featured_image_url} />
+				<div>{content}</div>
+			</div>
+		);
 	}
 }
