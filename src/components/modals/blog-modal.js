95c624a89d575b65aea7b55e247af0d5ddddeadf
Leak <sethpowellbusiness@gmail.com>
2021-09-01 14:27:03 -0700

    added modals folder and imported react-modal library and instantiated

---
+++ src/components/modals/blog-modal.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import ReactModal from "react-modal";
+
+export default class BlogModal extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<ReactModal isOpen={true}>
+				<h1>I'm in a modal</h1>
+			</ReactModal>
+		);
+	}
+}
