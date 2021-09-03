dafe7c4d2f39582817ececf4f4cd741ee702ac73
Leak <sethpowellbusiness@gmail.com>
2021-09-03 16:03:31 -0700

    customised toolbar of RichTextEditor

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -26,6 +26,10 @@ export default class RichTextEditor extends Component {
 		);
 	}
 
+	uploadFile(file) {
+		console.log("Upload file", file);
+	}
+
 	render() {
 		return (
 			<div className="">
@@ -34,6 +38,20 @@ export default class RichTextEditor extends Component {
 					wrapperClassName="demo-wrapper"
 					editorClassName="demo-editor"
 					onEditorStateChange={this.onEditorStateChange}
+					toolbar={{
+						inline: { inDropdown: true },
+						list: { inDropdown: true },
+						textAlign: { inDropdown: true },
+						link: { inDropdown: true },
+						history: { inDropdown: true },
+						image: {
+							uploadCallback: this.uploadFile,
+							alt: { present: true, mandatory: false },
+							previewImage: true,
+							inputAccept:
+								"image/gif,image/jpeg,image/jpg,image/png,image/svg",
+						},
+					}}
 				/>
 			</div>
 		);
