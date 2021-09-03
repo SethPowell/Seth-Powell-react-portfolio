3d71fd1e98e6a4d274bcc3c0fd421a763d4ab934
Leak <sethpowellbusiness@gmail.com>
2021-09-03 16:45:12 -0700

    started building out handleEditClick to edit a blog-detail by clicking on the title

--- src/components/Pages/blog-detail.js
+++ src/components/Pages/blog-detail.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import ReactHtmlParser from "react-html-parser";
 import BlogFeaturedImage from "../blog/blog-featured-image";
+import BlogForm from "../blog/blog-form";
 
 export default class BlogDetail extends Component {
 	constructor(props) {
@@ -10,7 +11,15 @@ export default class BlogDetail extends Component {
 		this.state = {
 			currentId: this.props.match.params.slug,
 			blogItem: {},
+			editMode: false,
 		};
+
+		this.handleEditClick = this.handleEditClick.bind(this);
+	}
+
+	handleEditClick() {
+		console.log("handleEditClick ran");
+		this.setState({ editMode: true });
 	}
 
 	getBlogItem() {
@@ -36,15 +45,23 @@ export default class BlogDetail extends Component {
 		const { title, content, featured_image_url, blog_status } =
 			this.state.blogItem;
 
-		return (
-			<div className="blog-container">
-				<div className="content-container">
-					<h1>{title}</h1>
-					<BlogFeaturedImage img={featured_image_url} />
+		const contentManager = () => {
+			if (this.state.editMode) {
+				return <BlogForm />;
+			} else {
+				return (
+					<div className="content-container">
+						<h1 onClick={this.handleEditClick}>{title}</h1>
+						<BlogFeaturedImage img={featured_image_url} />
 
-					<div className="content">{ReactHtmlParser(content)}</div>
-				</div>
-			</div>
-		);
+						<div className="content">
+							{ReactHtmlParser(content)}
+						</div>
+					</div>
+				);
+			}
+		};
+
+		return <div className="blog-container">{contentManager()}</div>;
 	}
 }
