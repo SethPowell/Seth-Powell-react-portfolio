435ab015f37ae1004f4c56eaff6b94240dcd461c
Leak <sethpowellbusiness@gmail.com>
2021-09-06 18:19:28 -0700

    added styles to about me page

--- src/components/Pages/about.js
+++ src/components/Pages/about.js
@@ -1,9 +1,25 @@
 import React from "react";
+import profilePicture from "../../../static/assets/images/bio/AllMenDie.png";
 
-export default function() {
-    return (
-    <div>
-        About Me
-    </div>
-    );
+export default function () {
+	return (
+		<div className="content-page-wrapper">
+			<div
+				className="left-column"
+				style={{
+					background: "url(" + profilePicture + ") no-repeat",
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+				}}
+			/>
+			<div className="right-column">
+				This is where my bio is gonna go Lorem ipsum dolor sit amet,
+				consectetur adipiscing elit. Suspendisse dignissim tristique
+				velit id hendrerit. In finibus condimentum bibendum. Nam
+				sollicitudin ante eget tempus imperdiet. Aenean feugiat interdum
+				turpis vel dictum. Vestibulum ornare ante odio, vel vehicula
+				enim accumsan vel.
+			</div>
+		</div>
+	);
 }
