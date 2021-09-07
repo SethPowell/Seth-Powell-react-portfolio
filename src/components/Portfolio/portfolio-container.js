fe1c3e9a4efeacf342f2ddc37c22ec5895232ae6
Leak <sethpowellbusiness@gmail.com>
2021-09-06 20:17:54 -0700

    fixed bug in homepage filter bar also added additional 'all' button

--- src/components/Portfolio/portfolio-container.js
+++ src/components/Portfolio/portfolio-container.js
@@ -1,73 +1,97 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import PortfolioItem from "./portfolio-item"
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
-        
-        this.state = {
-            pageTitle: "Welcome to my portfolio",
-            isLoading: false,
-            data: []
-        }
+	constructor() {
+		super();
 
-        this.handleFilter = this.handleFilter.bind(this);
-    }
+		this.state = {
+			pageTitle: "Welcome to my portfolio",
+			isLoading: false,
+			data: []
+		};
 
-    handleFilter(filter) {
-        this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
-        })
-    }
+		this.handleFilter = this.handleFilter.bind(this);
+	}
 
-    getPortfolioItems() {
-        axios
-          .get('https://sethpowell.devcamp.space/portfolio/portfolio_items')
-          .then(response => {
-            console.log("response data",response);
-            this.setState({
-                data: response.data.portfolio_items
-            })
-          })
-          .catch(error => {
-            console.log(error);
-          })
-    }
+	handleFilter(filter) {
+		if (filter === "CLEAR_FILTERS") {
+			this.getPortfolioItems();
+		} else {
+			this.getPortfolioItems(filter);
+		}
+	}
 
-    portfolioItems() {
-        return this.state.data.map(item => {
-            return (
-                <PortfolioItem 
-                    key={item.id} 
-                    item={item}
-                />
-            )
+	getPortfolioItems(filter = null) {
+		axios
+			.get("https://sethpowell.devcamp.space/portfolio/portfolio_items")
+			.then((response) => {
+				if (filter) {
+					this.setState({
+						data: response.data.portfolio_items.filter((item) => {
+							return item.category === filter;
+						})
+					});
+				} else {
+					this.setState({
+						data: response.data.portfolio_items
+					});
+				}
+			})
+			.catch((error) => {
+				console.log("error in getPortfolioItems: ", error);
+			});
+	}
 
-        })
-    }
+	portfolioItems() {
+		return this.state.data.map((item) => {
+			return <PortfolioItem key={item.id} item={item} />;
+		});
+	}
 
-    componentDidMount() {
-        this.getPortfolioItems();
+	componentDidMount() {
+		this.getPortfolioItems();
+	}
 
-    }
+	render() {
+		if (this.state.isLoading) {
+			return <div>Loading Content...</div>;
+		}
 
-    render() {
-        if (this.state.isLoading) {
-            return <div>Loading Content...</div>;
-        }
-
-        return (
-            <div className="portfolio-items-wrapper">
-                <button className="btn" onClick={() => this.handleFilter("Search")} >Search Engine</button>
-                <button className="btn" onClick={() => this.handleFilter("SocialMedia")} >Social Media</button>
-                <button className="btn" onClick={() => this.handleFilter("ConsumerTech")} >Consumer Tech</button>
-
-                {this.portfolioItems()}
-            </div>
-        );
-    }
+		return (
+			<div className="homepage-wrapper">
+				<div className="filter-links">
+					<button
+						className="btn"
+						onClick={() => this.handleFilter("Search")}
+					>
+						Search Engine
+					</button>
+					<button
+						className="btn"
+						onClick={() => this.handleFilter("SocialMedia")}
+					>
+						Social Media
+					</button>
+					<button
+						className="btn"
+						onClick={() => this.handleFilter("ConsumerTech")}
+					>
+						Consumer Tech
+					</button>
+					<button
+						className="btn"
+						onClick={() => this.handleFilter("CLEAR_FILTERS")}
+					>
+						All
+					</button>
+				</div>
+				<div className="portfolio-items-wrapper">
+					{this.portfolioItems()}
+				</div>
+			</div>
+		);
+	}
 }
