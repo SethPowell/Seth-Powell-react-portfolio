0c0ffaae8d5f775faf454a0044e3d0d44da85943
Leak <sethpowellbusiness@gmail.com>
2021-09-06 19:17:11 -0700

    added link to portfolioItem comp so it takes you to the portfolio detail for that item when clicked

--- src/components/Portfolio/portfolio-detail.js
+++ src/components/Portfolio/portfolio-detail.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
-export default function(props) {
-    return (
-        <div>
-            <h2>Portfolio Detail for {props.match.params.slug}</h2>
-        </div>
-    )
+export default class PortfolioDetail extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	componentDidMount() {
+		this.getPortfolioItem();
+	}
+
+	getPortfolioItem() {
+		axios
+			.get(
+				`https://sethpowell.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
+				{ withCredentials: true }
+			)
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.log("getPortfolioItem error: ", error);
+			});
+	}
+
+	render() {
+		return (
+			<div className="">
+				<h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+			</div>
+		);
+	}
 }
